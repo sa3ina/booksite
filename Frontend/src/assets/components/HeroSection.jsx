754df4c9221fe7df04ab1c7893a7ecbc491b491b
sa3ina <sravnennaya@mail.ru>
2024-07-24 21:02:07 +0400

    grainy effect,animation and stickers placement

--- Frontend/src/assets/components/HeroSection.jsx
+++ Frontend/src/assets/components/HeroSection.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-
+import gamification from "../images/svg-image-6.svg";
+import interaction from "../images/svg-image-16.svg";
+import usertesting from "../images/svg-image-17.svg";
+import marketing from "../images/svg-image-18.svg";
+import safety from "../images/svg-image-19.svg";
+import ui from "../images/svg-image-20.svg";
+import ux from "../images/svg-image-21.svg";
 const HeroSection = () => {
   return (
     <div className="hero">
@@ -19,10 +25,21 @@ const HeroSection = () => {
           <button className="buy">Buy Now</button>
         </div>
         <div className="heroright">
-          <img
-            src="https://framerusercontent.com/images/rLFzvpOs171d8YrWNHP31dlULM.png"
-            alt=""
-          />
+          <div className="bookcont">
+            <img
+              className="bookimage"
+              src="https://framerusercontent.com/images/rLFzvpOs171d8YrWNHP31dlULM.png"
+              alt=""
+            />
+
+            <img src={gamification} alt="" className="gamification sticker" />
+            <img src={interaction} alt="" className="interaction sticker" />
+            <img src={usertesting} alt="" className="usertesting sticker" />
+            <img src={marketing} alt="" className="marketing sticker" />
+            <img src={safety} alt="" className="safety sticker" />
+            <img src={ui} alt="" className="ui sticker" />
+            <img src={ux} alt="" className="ux sticker" />
+          </div>
         </div>
       </div>
     </div>
