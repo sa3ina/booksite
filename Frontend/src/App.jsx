0cdbc42522f08ce640347a09e569be5da95c7268
sa3ina <sravnennaya@mail.ru>
2024-07-26 18:01:17 +0400

    hero section little bit responsiveness

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routes } from "./routes/Route";
+import "normalize.css";
 function App() {
   const router = createBrowserRouter(routes);
   return <RouterProvider router={router} />;
