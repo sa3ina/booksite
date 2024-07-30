85b6be6d09dc197d9acdb692dca2703be9e7ed81
sa3ina <sravnennaya@mail.ru>
2024-07-30 17:00:24 +0400

    book pages section scroll

---
+++ Frontend/src/assets/components/BookPages.jsx
@@ -0,0 +1,172 @@
+import React from "react";
+
+const BookPages = () => {
+  return (
+    <div className="bookpages">
+      <div className="bookpagescont">
+        <div className="camera-roll">
+          <div className="line">
+            <img
+              src="https://framerusercontent.com/images/RIo9pe5dIRioAshVpUMRJvjRs5o.png?scale-down-to=1024"
+              alt="Image 1"
+            />
+            <img
+              src="https://framerusercontent.com/images/wpbjUFKUyj6ubEBp6YUgAbu8udI.png?scale-down-to=1024"
+              alt="Image 2"
+            />
+            <img
+              src="https://framerusercontent.com/images/gy1MS3JNH51cuEbsBGciaCoqPg.png?scale-down-to=1024"
+              alt="Image 3"
+            />
+            <img
+              src="https://framerusercontent.com/images/0KkhObK15zPSOl4Vg4PCcQsgGxM.png?scale-down-to=1024"
+              alt="Image 4"
+            />
+            <img
+              src="https://framerusercontent.com/images/OZtG339epfimrOs3lxkrmao7YQE.png?scale-down-to=1024"
+              alt="Image 5"
+            />
+            <img
+              src="https://framerusercontent.com/images/DgrNij9fGnniCOjGiJ8u5mAB9Dg.png?scale-down-to=1024"
+              alt="Image 6"
+            />
+            <img
+              src="https://framerusercontent.com/images/RIo9pe5dIRioAshVpUMRJvjRs5o.png?scale-down-to=1024"
+              alt="Image 1"
+            />
+            <img
+              src="https://framerusercontent.com/images/wpbjUFKUyj6ubEBp6YUgAbu8udI.png?scale-down-to=1024"
+              alt="Image 2"
+            />
+            <img
+              src="https://framerusercontent.com/images/gy1MS3JNH51cuEbsBGciaCoqPg.png?scale-down-to=1024"
+              alt="Image 3"
+            />
+            <img
+              src="https://framerusercontent.com/images/0KkhObK15zPSOl4Vg4PCcQsgGxM.png?scale-down-to=1024"
+              alt="Image 4"
+            />
+            <img
+              src="https://framerusercontent.com/images/OZtG339epfimrOs3lxkrmao7YQE.png?scale-down-to=1024"
+              alt="Image 5"
+            />
+            <img
+              src="https://framerusercontent.com/images/DgrNij9fGnniCOjGiJ8u5mAB9Dg.png?scale-down-to=1024"
+              alt="Image 6"
+            />
+          </div>
+          <div className="line reverse">
+            <img
+              src="https://framerusercontent.com/images/nJkzxvv6XKxUEjjhVCEixinhM.png?scale-down-to=1024"
+              alt="Image 7"
+            />
+            <img
+              src="https://framerusercontent.com/images/csgQEjfKf067KHhkiYsh6ygNlgU.png?scale-down-to=1024"
+              alt="Image 8"
+            />
+            <img
+              src="https://framerusercontent.com/images/ky8vek5xBivuTyDU8JEf8lnGAA0.png?scale-down-to=1024"
+              alt="Image 9"
+            />
+            <img
+              src="https://framerusercontent.com/images/fDRZDPwPjXusgZ6MOsEuZ5tQ.png?scale-down-to=1024"
+              alt="Image 10"
+            />
+            <img
+              src="https://framerusercontent.com/images/TvKBfJAunuACr4hd3hFiYfvros.png?scale-down-to=1024"
+              alt="Image 11"
+            />
+            <img
+              src="https://framerusercontent.com/images/TId4WHPMFqlgw0ul03udOmaAH8.png?scale-down-to=1024"
+              alt="Image 12"
+            />
+            <img
+              src="https://framerusercontent.com/images/nJkzxvv6XKxUEjjhVCEixinhM.png?scale-down-to=1024"
+              alt="Image 7"
+            />
+            <img
+              src="https://framerusercontent.com/images/csgQEjfKf067KHhkiYsh6ygNlgU.png?scale-down-to=1024"
+              alt="Image 8"
+            />
+            <img
+              src="https://framerusercontent.com/images/ky8vek5xBivuTyDU8JEf8lnGAA0.png?scale-down-to=1024"
+              alt="Image 9"
+            />
+            <img
+              src="https://framerusercontent.com/images/fDRZDPwPjXusgZ6MOsEuZ5tQ.png?scale-down-to=1024"
+              alt="Image 10"
+            />
+            <img
+              src="https://framerusercontent.com/images/TvKBfJAunuACr4hd3hFiYfvros.png?scale-down-to=1024"
+              alt="Image 11"
+            />
+            <img
+              src="https://framerusercontent.com/images/TId4WHPMFqlgw0ul03udOmaAH8.png?scale-down-to=1024"
+              alt="Image 12"
+            />
+          </div>
+          <div className="line">
+            <img
+              src="https://framerusercontent.com/images/Ikjllfjnf41BlWQQP8B9pqTbFM.png?scale-down-to=1024"
+              alt="Image 13"
+            />
+            <img
+              src="https://framerusercontent.com/images/jFENWa5SnIE7fxo18E2QuFEbom4.png?scale-down-to=1024"
+              alt="Image 14"
+            />
+            <img
+              src="https://framerusercontent.com/images/EHn1NKZE24V9lKiyKWTiWUOlU9Y.png?scale-down-to=1024"
+              alt="Image 15"
+            />
+            <img
+              src="https://framerusercontent.com/images/yP6SPyTt2IiQ3jI5SVB3wQ33uMk.png?scale-down-to=1024"
+              alt="Image 16"
+            />
+            <img
+              src="https://framerusercontent.com/images/DgrNij9fGnniCOjGiJ8u5mAB9Dg.png?scale-down-to=1024"
+              alt="Image 17"
+            />
+            <img
+              src="https://framerusercontent.com/images/jJCQzJnCPfsKklPSoAooz4cxg.png?scale-down-to=1024"
+              alt="Image 18"
+            />
+            <img
+              src="https://framerusercontent.com/images/Ikjllfjnf41BlWQQP8B9pqTbFM.png?scale-down-to=1024"
+              alt="Image 13"
+            />
+            <img
+              src="https://framerusercontent.com/images/jFENWa5SnIE7fxo18E2QuFEbom4.png?scale-down-to=1024"
+              alt="Image 14"
+            />
+            <img
+              src="https://framerusercontent.com/images/EHn1NKZE24V9lKiyKWTiWUOlU9Y.png?scale-down-to=1024"
+              alt="Image 15"
+            />
+            <img
+              src="https://framerusercontent.com/images/yP6SPyTt2IiQ3jI5SVB3wQ33uMk.png?scale-down-to=1024"
+              alt="Image 16"
+            />
+            <img
+              src="https://framerusercontent.com/images/DgrNij9fGnniCOjGiJ8u5mAB9Dg.png?scale-down-to=1024"
+              alt="Image 17"
+            />
+            <img
+              src="https://framerusercontent.com/images/jJCQzJnCPfsKklPSoAooz4cxg.png?scale-down-to=1024"
+              alt="Image 18"
+            />
+          </div>
+        </div>
+        <div className="right">
+          <p className="full">
+            <b>A full compendium</b> covering research, development, UX design,
+            UI design, interaction, animation, character design, user testing
+            and marketing.
+          </p>
+          <button className="buypurple">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BookPages;
--- Frontend/src/pages/Main.jsx
+++ Frontend/src/pages/Main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "../assets/styles/main.scss";
 import HeroSection from "../assets/components/HeroSection";
+import BookPages from "../assets/components/BookPages";
 const Main = () => {
   return (
     <div className="main">
       <HeroSection />
+      <BookPages />
     </div>
   );
 };
